4a2c62cb2dee97fda15ddd41604ccc2c55a9ac74
AFKoala <bubbles.murph@gmail.com>
2017-08-30 12:51:15 -0400

    adding math

--- verify2.rb
+++ verify2.rb
@@ -1,48 +1,59 @@
-def isbn_dash(isbn)
-    puts isbn
-    x = 10
-    clean_isbn = isbn.delete('-')
-    puts clean_isbn
-    unless isbn || (isbn.include?(x) && isbn[9] == x)
-        false
-    end
+# def isbn_dash(isbn)
+#     puts isbn
+#     x = 10
+#     clean_isbn = isbn.delete('-')
+#     puts clean_isbn
+#     case clean_isbn.length
+#         when 10
+#             then true
+#         when 13
+#             then true
+#     end
+#     unless isbn || (isbn.include?(x) && isbn[9] == x)
+#         false
+#     end
+#     checksum = clean_isbn.pop
+#     puts checksum
+# end
 
+# def isbn_space(isbn)
+#     # puts isbn
+#     clean_isbn = isbn.delete(' ')
+#     # puts clean_isbn
+#     case clean_isbn.length
+#         when 10
+#             then true
+#         when 13
+#             then true
+#     end
+#     unless isbn || (isbn.include?(x) && isbn[9] == x)
+#         false
+#     end
+#     checksum = clean_isbn.pop
+#     puts checksum
+# end
+
+def isbn_both(isbn)
+    puts isbn
+    clean_isbn = isbn.delete(' -')
+    puts clean_isbn
     case clean_isbn.length
         when 10
             then true
         when 13
             then true
     end
-end
-
-def isbn_space(isbn)
-    # puts isbn
-    clean_isbn = isbn.delete(' ')
-    # puts clean_isbn
-
-   case clean_isbn.length
-        when 10
-            then true
-        when 13
-            then true
+    unless isbn || (isbn.include?(x) && isbn[9] == x)
+        false
     end
+    split_isbn = clean_isbn.scan
+    puts split_isbn
+    checksum = split_isbn.pop
+    puts checksum
 end
 
-def isbn_both(isbn)
-    puts isbn
-    clean_isbn = isbn.delete(' -')
-    puts clean_isbn
-
-   case clean_isbn.length
-        when 10
-            then true
-        when 13
-            then true
-    end
-end
-
-isbn_dash('1234-567-89x')
-puts '------------'
-puts '------------'
-isbn_space('1234 567 890')
+# isbn_dash('1234-567-89x')
+# puts '------------'
+# isbn_space('1234 567 890')
+# puts '------------'
 isbn_both('1-234 567 8-90')
