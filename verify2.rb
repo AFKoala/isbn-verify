eb1888d673ec24f4830e930c90c91d7ee4949458
AFKoala <bubbles.murph@gmail.com>
2017-08-31 15:26:18 -0400

    working

--- verify2.rb
+++ verify2.rb
@@ -34,26 +34,45 @@
 # end
 
 def isbn_both(isbn)
-    puts isbn
+    p isbn
     clean_isbn = isbn.delete(' -')
-    puts clean_isbn
+    p clean_isbn
     case clean_isbn.length
         when 10
             then true
         when 13
             then true
     end
-    unless isbn || (isbn.include?(x) && isbn[9] == x)
-        false
-    end
-    split_isbn = clean_isbn.scan
-    puts split_isbn
+    # p clean_isbn.class
+    split_isbn = clean_isbn.chars
+    # p split_isbn.class
+    p split_isbn
     checksum = split_isbn.pop
-    puts checksum
+    p checksum
+    if clean_isbn.length == 10 && checksum == 'x'
+        checksum.sub!('x', '10')
+    end
+    checksum = checksum.to_i
+    p checksum
+    if clean_isbn.length == 10
+        check = (1*isbn[0].to_i) + (2*isbn[1].to_i) + (3*isbn[2].to_i) + (4*isbn[3].to_i) + (5*isbn[4].to_i) + (6*isbn[5].to_i) + (7*isbn[6].to_i) + (8*isbn[7].to_i) + (9*isbn[8].to_i)
+        final = check%11
+        p final
+        if final == checksum
+            puts "VALID"
+        else
+            puts "INVALID"
+        end        
+    elsif clean_isbn.length == 13
+        check = (1*isbn[0].to_i) + (3*isbn[1].to_i) + (1*isbn[2].to_i) + (3*isbn[3].to_i) + (1*isbn[4].to_i) + (3*isbn[5].to_i) + (1*isbn[6].to_i) + (3*isbn[7].to_i) + (1*isbn[8].to_i) + (3*isbn[9].to_i) + (1*isbn[10].to_i) + (3*isbn[11].to_i) + (1*isbn[12].to_i)
+        final = check%10
+        p final
+        if final == checksum
+            puts "VALID"
+        else
+            puts "INVALID"
+        end 
+    end        
 end
 
-# isbn_dash('1234-567-89x')
-# puts '------------'
-# isbn_space('1234 567 890')
-# puts '------------'
-isbn_both('1-234 567 8-90')
+isbn_both('9780470059029')
