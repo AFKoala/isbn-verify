8cfc693c13eee1457fa75fd11d8e0c2dac1d1655
AFKoala <bubbles.murph@gmail.com>
2017-09-12 13:13:20 -0400

    Delete local_env.rb

--- local_env.rb
+++
@@ -1,6 +0,0 @@
-ENV['AWS_ACCESS_KEY_ID'] = 'AKIAIHD7ZTEGBZZXCBKA'
-ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-ENV['AWS-REGION'] = 'us-east-2'
-ENV['PATH_STYLE'] = 'true'
-ENV['S3_FILE'] = 'testfile2.txt'
-ENV['bucket'] = 'minedmindskata'
