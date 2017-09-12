bc7e261150e581d5b92db97c467c1100cf719efe
AFKoala <bubbles.murph@gmail.com>
2017-09-12 13:10:10 -0400

    hiding credentials

--- local_env.rb
+++ local_env.rb
@@ -1,4 +1,6 @@
 ENV['AWS_ACCESS_KEY_ID'] = 'AKIAIHD7ZTEGBZZXCBKA'
 ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-ENV['AWS-REGION'] = 'us-east'
-ENV['PATH_STYLE']='true'
+ENV['AWS-REGION'] = 'us-east-2'
+ENV['PATH_STYLE'] = 'true'
+ENV['S3_FILE'] = 'testfile2.txt'
+ENV['bucket'] = 'minedmindskata'
