ef91d7e0f61b69f30d1d37642019d21888c05db8
AFKoala <bubbles.murph@gmail.com>
2017-09-15 14:10:10 -0400

    many updates

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 source :rubygems
 ruby '2.4.1'
 gem 'sinatra', '2.0.0'
+gem 'aws-sdk', '3.0.1'
--- learn.rb
+++ learn.rb
@@ -2,18 +2,21 @@ require 'rubygems'
 require 'aws-sdk'
 require 'csv'
   load "./local_env.rb" 
-def connect_to_s3()
+def send_to_bucket()
  Aws::S3::Client.new(
     access_key_id: ENV['AWS_ACCSESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: ENV['AWS_REGION'],
-  
   )
-  file = "testfile.txt"
+  file = 'input_isbn_file.csv'
+  write_to_file = File.open(file, 'a')
+  write_to_file << bucketlist + '\n'
+  write_to_file.close
+
+  s3 = Aws::S3::Resource.new(region: 'us-east-2')
   bucket = 'minedmindskata'
-  s3 = Aws::S3::Resource.new(region: 'us-east')
-  obj = s3.bucket(bucket).object(file)
-  # string data
-    obj.put(body: '"some code here to show something being added to the bucket."+ "\n"')
-    # push entire file 
+  obj = s3.bucket(bucket).object('input_isbn_file.csv')
+    File.open('input_isbn_file.csv', 'rb') do |file|
+      obj.put(body: file)
+    end
 end
