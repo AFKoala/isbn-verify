f2a712b83f823e1b4baea9056f32cdd841fe9b84
AFKoala <bubbles.murph@gmail.com>
2017-09-15 09:35:11 -0400

    fixed security issues

--- verify.rb
+++ verify.rb
@@ -1,40 +1,177 @@
-def isbn10(isbn)
-    puts "running ISBN10"
-    puts isbn
-    isbn.is_a? String
-    x = 10
-    puts "----------"
-    isbn = isbn.gsub(/[ -abdcefghijklmnopqrstuvwyz]/, '')
-    isbn =  isbn.split(//)
-    checksum = isbn.pop
-    puts isbn
-    #puts checksum
-    check = (1*isbn[0].to_i) + (2*isbn[1].to_i) + (3*isbn[2].to_i) + (4*isbn[3].to_i) + (5*isbn[4].to_i) + (6*isbn[5].to_i) + (7*isbn[6].to_i) + (8*isbn[7].to_i) + (9*isbn[8].to_i)
-    puts check
-    final = check%11
-    puts final
-    if final == 10
-        puts "VALID"
+# def isbn10(isbn)
+#     puts "running ISBN10"
+#     puts isbn
+#     isbn.is_a? String
+#     x = 10
+#     puts "----------"
+#     isbn = isbn.gsub(/[ -abdcefghijklmnopqrstuvwyz]/, '')
+#     isbn =  isbn.split(//)
+#     checksum = isbn.pop
+#     puts isbn
+#     #puts checksum
+#     check = (1*isbn[0].to_i) + (2*isbn[1].to_i) + (3*isbn[2].to_i) + (4*isbn[3].to_i) + (5*isbn[4].to_i) + (6*isbn[5].to_i) + (7*isbn[6].to_i) + (8*isbn[7].to_i) + (9*isbn[8].to_i)
+#     puts check
+#     final = check%11
+#     puts final
+#     if final == 10
+#         puts "VALID"
+#     else
+#         puts "INVALID"
+#     end
+    
+# end
+
+# def isbn13(isbn)
+#     puts "running ISBN13"
+#     isbn_final =  isbn_new.split(//)
+#     checksum = isbn_new.pop
+#     #puts checksum
+#     check = (1*isbn[0].to_i) + (3*isbn[1].to_i) + (1*isbn[2].to_i) + (3*isbn[3].to_i) + (1*isbn[4].to_i) + (3*isbn[5].to_i) + (1*isbn[6].to_i) + (3*isbn[7].to_i) + (1*isbn[8].to_i) + (3*isbn[9].to_i) + (1*isbn[10].to_i) + (3*isbn[11].to_i) + (1*isbn[12].to_i)
+#     #puts check
+#     sum = check%10
+#     #puts sum
+#     if sum == checksum
+#         puts "VALID"
+#     else
+#         puts "INVALID"
+#     end
+# end
+
+# isbn10("1234567890")
+
+require 'csv'
+
+def isbn_function(user_given_isbn)
+    no_space_or_dash_isbn = user_given_isbn.delete(' -')
+
+    answer_array = []
+
+    if no_space_or_dash_isbn !~ /\D/
+        
+        if no_space_or_dash_isbn.length == 10
+            isbn_array = no_space_or_dash_isbn.split('')
+            last_digit = isbn_array.pop
+            
+            isbn_array.each_with_index do |value, index_position|
+                sum = (index_position.to_i + 1) * value.to_i
+                answer_array << sum
+            end
+
+            sum_answer = answer_array.inject(0, :+)
+            mod_answer = sum_answer % 11
+
+                if mod_answer == last_digit.to_i
+                    return_variable = true
+            
+                else
+                    return_variable = false
+                end
+
+        elsif no_space_or_dash_isbn.length == 13
+            isbn_array = no_space_or_dash_isbn.split('')
+            last_digit = isbn_array.pop
+            
+            isbn_array.each_with_index do |value, index_position|
+                if index_position.to_i % 2 == 0
+                    sum = value.to_i * 1
+                    answer_array << sum
+              
+                else
+                    sum = value.to_i * 3
+                    answer_array << sum
+                end
+            end
+
+            sum_answer = answer_array.inject(0, :+)
+            mod_answer = (10 - (sum_answer % 10)) % 10
+                if mod_answer == last_digit.to_i
+                    return_variable = true
+                
+                else
+                    return_variable = false
+                end
+
+        else
+            return_variable = false
+        end
+
+    elsif no_space_or_dash_isbn.chop !~ /\D/
+        if no_space_or_dash_isbn.length == 10
+            isbn_array = no_space_or_dash_isbn.split('')
+            last_digit = isbn_array.pop
+     
+            isbn_array.each_with_index do |value, index_position|
+            sum = ((index_position.to_i + 1) * value.to_i)
+            answer_array << sum
+        end
+
+        sum_answer = answer_array.inject(0, :+)
+        mod_answer = sum_answer % 11
+
+        if mod_answer <= 9
+            mod_answer   
+        
+        elsif mod_answer == 10
+            mod_answer = "x"
+        
+        else
+            return_variable = false
+        end
+
+        if mod_answer == last_digit.downcase
+            return_variable = true
+        
+        else
+            return_variable = false
+        end
+    
+    elsif no_space_or_dash_isbn.length == 13
+        isbn_array = no_space_or_dash_isbn.split('')
+        last_digit = isbn_array.pop
+        
+        isbn_array.each_with_index do |value, index_position|
+            if index_position.to_i % 2 == 0
+                sum = value.to_i * 1
+                answer_array << sum
+            
+            else
+                sum = value.to_i * 3
+                answer_array << sum
+            end
+        end
+
+        sum_answer = answer_array.inject(0, :+)
+        mod_answer = (10 - (sum % 10)) % 10
+            if mod_answer == last_digit.to_i
+                return_variable = true
+            
+            else
+                return_variable = false
+            end
+
+        else
+            return_variable = false
+        end
+    
     else
-        puts "INVALID"
+        return_variable = false
     end
     
+    return_variable
 end
 
-def isbn13(isbn)
-    puts "running ISBN13"
-    isbn_final =  isbn_new.split(//)
-    checksum = isbn_new.pop
-    #puts checksum
-    check = (1*isbn[0].to_i) + (3*isbn[1].to_i) + (1*isbn[2].to_i) + (3*isbn[3].to_i) + (1*isbn[4].to_i) + (3*isbn[5].to_i) + (1*isbn[6].to_i) + (3*isbn[7].to_i) + (1*isbn[8].to_i) + (3*isbn[9].to_i) + (1*isbn[10].to_i) + (3*isbn[11].to_i) + (1*isbn[12].to_i)
-    #puts check
-    sum = check%10
-    #puts sum
-    if sum == checksum
-        puts "VALID"
-    else
-        puts "INVALID"
+def check_csv
+    write_file = File.open("output_isbn_file.csv", "w")
+
+    CSV.foreach('input_isbn_file.csv') do |row|
+        if isbn_function(row[1]) == true
+            row << ("valid")
+        else
+            row << ("invalid")
+        end
+
+        write_file.puts row[0] + ", " + row[1] + ", " + row[2]
     end
 end
 
-isbn10("1234567890")
+check_csv
