a0b9c6beb7738b57283898fea4ba2cfb86383fd7
AFKoala <bubbles.murph@gmail.com>
2017-09-15 16:06:10 -0400

    things are happening

--- verify.rb
+++ verify.rb
@@ -121,6 +121,25 @@ def isbn_function(user_given_isbn)
     return_variable
 end
 
+def send_to_bucket()
+    Aws::S3::Client.new(
+       access_key_id: ENV['AWS_ACCSESS_KEY_ID'],
+       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+       region: ENV['AWS_REGION'],
+     )
+     file = 'input_isbn_file.csv'
+     write_to_file = File.open(file, 'a')
+     write_to_file << bucketlist + '\n'
+     write_to_file.close
+   
+     s3 = Aws::S3::Resource.new(region: 'us-east-2')
+     bucket = 'minedmindskata'
+     obj = s3.bucket(bucket).object('input_isbn_file.csv')
+       File.open('input_isbn_file.csv', 'rb') do |file|
+         obj.put(body: file)
+       end
+   end
+
 def get_file()
     s3 = Aws::S3::Client.new
     csv_file_from_bucket = s3.get_object(bucket: 'minedmindskata', key: 'new.csv')
